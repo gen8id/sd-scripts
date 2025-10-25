5ffcfb27ed6de83d6148180831b39539a91e4ba6
sungjoonkim <ted@bitflow.ai>
2025-10-25 23:08:18 +0900

    aaa

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ mkdir -p /app/sdxl_train_captioner/dataset/captioning/background
 mkdir -p /app/sdxl_train_captioner/dataset/captioning/mainchar
 mkdir -p /app/sdxl_train_captioner/dataset/training/background
 mkdir -p /app/sdxl_train_captioner/dataset/training/mainchar
+mkdir -p /app/sdxl_train_captioner/dataset/training/mainchar
+mkdir -p /app/sdxl_train_captioner/output_model
+mkdir -p /app/sdxl_train_captioner/logs
 
 # 모델 다운로드
 if [ ! -f /app/sdxl_train_captioner/models/sd_xl_base_1.0.safetensors ]; then
