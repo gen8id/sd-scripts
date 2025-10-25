cbe231a17a257baa9c716b56208e891508cfb6b6
sungjoonkim <ted@bitflow.ai>
2025-10-25 23:29:36 +0900

    fdfdfd

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,6 @@ mkdir -p /app/sdxl_train_captioner/logs
 if [ ! -f /app/sdxl_train_captioner/models/sd_xl_base_1.0.safetensors ]; then
     wget -O /app/sdxl_train_captioner/models/sd_xl_base_1.0.safetensors https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
 fi
+
+cd /app/sdxl_train_captioner/sd-scripts
+python cap-watcher.py
