2dc567b1df55a46c32d474b45aac03bb6bb6bcb0
sungjoonkim <ted@bitflow.ai>
2025-10-25 22:19:28 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+mkdir -p /app/sdxl_train_captioner/dataset/captioning/background
+mkdir -p /app/sdxl_train_captioner/dataset/captioning/mainchar
+mkdir -p /app/sdxl_train_captioner/dataset/training/background
+mkdir -p /app/sdxl_train_captioner/dataset/training/mainchar
+
 # 모델 다운로드
 if [ ! -f /app/sdxl_train_captioner/models/sd_xl_base_1.0.safetensors ]; then
     wget -O /app/sdxl_train_captioner/models/sd_xl_base_1.0.safetensors https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
